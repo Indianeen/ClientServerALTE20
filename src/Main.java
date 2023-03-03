8be6557e55ab110e1ae7327e84c81222bb0a94db
Indianeen <axel.lundberg@elev.ga.ntig.se>
2023-03-03 12:33:16 +0100

    core in main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import Server.Core;
 
- //core=controller
+//core=controller
 public class Main {
     public static void main(String[] args) {
-
+        Core serCor = new Core();
+        Client.Core cliCor = new Client.Core();
     }
 }
